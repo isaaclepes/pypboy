473cac1043409d3b0a35345c9082e3028b6e4597
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-24 22:39:16 +1300

    .

--- pypboy/modules/items/__init__.py
+++ pypboy/modules/items/__init__.py
@@ -4,6 +4,7 @@ from pypboy.modules.items import apparel
 from pypboy.modules.items import aid
 from pypboy.modules.items import misc
 from pypboy.modules.items import ammo
+import config
 
 
 class Module(BaseModule):
@@ -26,20 +27,20 @@ class Module(BaseModule):
 		self.pypboy.header.title = " Wg 186/280  |  HP 160/175"
 		self.active.handle_action("resume")
 
-	def render(self):
+	def render(self, interval):
 		new_date = datetime.datetime.now().strftime("%d.%m.%y.%H:%M:%S")
-		if new_date != self._date:
-			self.image.fill((0, 0, 0))
-			pygame.draw.line(self.image, (95, 255, 177), (5, 15), (5, 35), 2)
-			pygame.draw.line(self.image, (95, 255, 177), (5, 15), (config.WIDTH - 154, 15), 2)
-			pygame.draw.line(self.image, (95, 255, 177), (config.WIDTH - 154, 15), (config.WIDTH - 154, 35), 2)
-			pygame.draw.line(self.image, (95, 255, 177), (config.WIDTH - 148, 15), (config.WIDTH - 13, 15), 2)
-			pygame.draw.line(self.image, (95, 255, 177), (config.WIDTH - 13, 15), (config.WIDTH - 13, 35), 2)
+		if new_date != self.header._date:
+			self.header.image.fill((0, 0, 0))
+			pygame.draw.line(self.header.image, (95, 255, 177), (5, 15), (5, 35), 2)
+			pygame.draw.line(self.header.image, (95, 255, 177), (5, 15), (config.WIDTH - 154, 15), 2)
+			pygame.draw.line(self.header.image, (95, 255, 177), (config.WIDTH - 154, 15), (config.WIDTH - 154, 35), 2)
+			pygame.draw.line(self.header.image, (95, 255, 177), (config.WIDTH - 148, 15), (config.WIDTH - 13, 15), 2)
+			pygame.draw.line(self.header.image, (95, 255, 177), (config.WIDTH - 13, 15), (config.WIDTH - 13, 35), 2)
 
-			text = config.FONTS[14].render("  %s  " % self.headline, True, (105, 251, 187), (0, 0, 0))
-			self.image.blit(text, (26, 8))
+			text = config.FONTS[14].render("  %s  " % self.header.headline, True, (105, 251, 187), (0, 0, 0))
+			self.header.image.blit(text, (26, 8))
 			text = config.FONTS[14].render("TEST", True, (95, 255, 177), (0, 0, 0))
-			self.image.blit(text, ((config.WIDTH - 154) - text.get_width() - 10, 19))
-			text = config.FONTS[14].render(self._date, True, (95, 255, 177), (0, 0, 0))
-			self.image.blit(text, ((config.WIDTH - 141), 19))
-			self._date = new_date
+			self.header.image.blit(text, ((config.WIDTH - 154) - text.get_width() - 10, 19))
+			text = config.FONTS[14].render(self.header._date, True, (95, 255, 177), (0, 0, 0))
+			self.header.image.blit(text, ((config.WIDTH - 141), 19))
+			self.header._date = new_date
