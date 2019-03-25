3f244959308fd7fb683fecc34e2267988687d0f0
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-25 22:51:44 +1300

    .

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -21,3 +21,15 @@ class Module(pypboy.SubModule):
 		
 	def change_items(self):
 		print "Changing"
+		
+class Weapon:
+	
+	def __init__(self, name, imageloc, damage, weight, value, condition, notes): 
+		self.name = name
+		self.imageloc = imageloc
+		self.damage = damage
+		self.weight= weight
+		self.value = value
+		self.condition = condition
+		self.notes = notes
+		
