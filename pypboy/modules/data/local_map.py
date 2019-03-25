469bb2e8820ff12f62df4d06112e749813eba540
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-25 19:11:00 +1300

    .

--- pypboy/modules/data/local_map.py
+++ pypboy/modules/data/local_map.py
@@ -23,8 +23,3 @@ class Module(pypboy.SubModule):
 		self.add(mapgrid)
 		mapgrid.rect[0] = 4
 		mapgrid.rect[1] = 40
-
-#	def handle_resume(self):
-#		self.parent.pypboy.header.headline = "DATA"
-#		self.parent.pypboy.header.title = "City Centre"
-#		super(Module, self).handle_resume()
--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -11,7 +11,3 @@ class Module(pypboy.SubModule):
 	def __init__(self, *args, **kwargs):
 		super(Module, self).__init__(*args, **kwargs)
 		
-#	def handle_resume(self):
-#		self.parent.pypboy.header.headline = "ITEMS"
-#		self.parent.pypboy.header.title = " Wg 186/280  |  HP 160/175"
-#		super(Module, self).handle_resume()
