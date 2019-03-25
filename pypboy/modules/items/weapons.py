b0b45e963c5698915df2e96128489d6f7c7736a1
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-25 19:58:06 +1300

    .

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -13,10 +13,20 @@ class Module(pypboy.SubModule):
 		handlers = []
 		for i in config.INVENTORY:
 			handlers.add(self.change_items)
-		self.menu = pypboy.ui.Menu(100, config.INVENTORY, handlers, 0)
+		#self.menu = pypboy.ui.Menu(100, config.INVENTORY, handlers, 0)
+		self.menu = pypboy.ui.Menu(100, ["CND", "RAD", "EFF"], [self.show_cnd, self.show_rad, self.show_eff], 0)
 		self.menu.rect[0] = 4
 		self.menu.rect[1] = 60
 		self.add(self.menu)
+
+	def show_cnd(self):
+		print "CND"
+
+	def show_rad(self):
+		print "RAD"
+
+	def show_eff(self):
+		print "EFF"
 		
 	def change_items(self):
 		print "Changing"
