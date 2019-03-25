62f0b57597be506df2de6211635baadfe4947522
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-25 18:48:48 +1300

    .

--- pypboy/modules/items/__init__.py
+++ pypboy/modules/items/__init__.py
@@ -13,6 +13,7 @@ class Module(BaseModule):
 
 	label = "ITEMS"
 	GPIO_LED_ID = 29 #GPIO27 #21
+	HeaderGap = 13
 
 	def __init__(self, *args, **kwargs):
 		self.submodules = [
@@ -45,12 +46,18 @@ class Module(BaseModule):
 			#self.pypboy.header.image.blit(text, ((config.WIDTH - 180) - text.get_width() - 10, 19))
 			#text = config.FONTS[14].render("HP  291/340", True, (95, 255, 177), (0, 0, 0))
 			#self.pypboy.header.image.blit(text, ((config.WIDTH - 168) - text.get_width() - 10, 19))
-			
+			headerposcount = 154
 			#Date
 			text = config.FONTS[14].render(self.pypboy.header._date, True, (95, 255, 177), (0, 0, 0))
 			self.pypboy.header.image.blit(text, ((config.WIDTH - 141), 19))
 			#DR
 			text = config.FONTS[14].render("DR  20", True, (95, 255, 177), (0, 0, 0))
-			self.pypboy.header.image.blit(text, ((config.WIDTH - 154) - text.get_width() - 10, 19))
+			headerposcount = headerposcount + text.get_width()
+			self.pypboy.header.image.blit(text, (config.WIDTH - headerposcount, 19))
+			
+			#HP
+			text = config.FONTS[14].render("HP  291/340", True, (95, 255, 177), (0, 0, 0))
+			headerposcount = headerposcount + text.get_width() + HeaderGap
+			self.pypboy.header.image.blit(text, (config.WIDTH - headerposcount, 19))
 			
 			self.pypboy.header._date = new_date
