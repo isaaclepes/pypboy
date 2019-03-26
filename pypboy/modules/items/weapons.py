6b4fa983b6984f6f2007d995212f32724a211ca6
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-26 20:01:36 +1300

    .

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -6,32 +6,32 @@ import config
 class Module(pypboy.SubModule):
 
 	label = " Weapons "
-	selected = 3
-	INVENTORY = [
-		Weapon('Chinese Assault Rifle','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Combat Shotgun','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Deathclaw Gauntlet','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Flamer','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Hunting Rifle','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Minigun','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Missile Launcher','images/inventory/flamer.png',0,0,0,0,''),
-		Weapon('Pulse Grenade (2)','images/inventory/flamer.png',0,0,0,0,'')
-	]
 
 	def __init__(self, *args, **kwargs):
 		super(Module, self).__init__(*args, **kwargs)
 		handlers = []
 		item_names = []
-		for i in self.INVENTORY:
+		INVENTORY = [
+			Weapon('Chinese Assault Rifle','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Combat Shotgun','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Deathclaw Gauntlet','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Flamer','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Hunting Rifle','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Minigun','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Missile Launcher','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Pulse Grenade (2)','images/inventory/flamer.png',0,0,0,0,'')
+		]
+		selected = 3
+		for i in INVENTORY:
 			print "%s" % (i.name)
 			handlers.append(self.change_items)
 			item_names.append(i.name)
-		self.menu = pypboy.ui.Menu(200, item_names, handlers, self.selected, 15)
+		self.menu = pypboy.ui.Menu(200, item_names, handlers, selected, 15)
 		self.menu.rect[0] = 4
 		self.menu.rect[1] = 60
 		self.add(self.menu)
 		#show weapon image
-		weapon_to_display = self.INVENTORY[self.selected]
+		weapon_to_display = INVENTORY[selected]
 		weapon_to_display.rect = weapon_to_display.image.get_rect()
 		weapon_to_display.image = weapon_to_display.image.convert()
 		weapon_to_display.rect[0] = 200
