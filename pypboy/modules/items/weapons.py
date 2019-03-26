e4fe8ffc9f6b7b65f2a3a4567584291ecaa40bf8
Goldstein <32555045+EricGoldsteinNz@users.noreply.github.com>
2019-03-26 21:43:33 +1300

    .

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -42,9 +42,9 @@ class Module(pypboy.SubModule):
 		
 		#Show Weapon stats - Value
 		text = config.FONTS[14].render("%s" %(weapon_to_display.value), True, (95, 255, 177), (0, 0, 0))
-		pygame.draw.line(weapon_to_display.image, (95, 255, 177), (config.WIDTH - 13, weapon_to_display.rect[1] + weapon_to_display.rect[3] + 5 ), (config.WIDTH - 13, weapon_to_display.rect[1] + weapon_to_display.rect[3] + 25), 2)	#End of title Verticle bar
+		pygame.draw.line(weapon_to_display.image, (95, 255, 177), (config.WIDTH - 13, weapon_to_display.rect[1] + weapon_to_display.image.get_rect()[3] + 5 ), (config.WIDTH - 13, weapon_to_display.rect[1] + weapon_to_display.image.get_rect()[3] + 25), 2)	#End of title Verticle bar
 		weapon_to_display.image.blit(text, (config.WIDTH - (text.get_width() + 5), 19))
-		pygame.draw.line(weapon_to_display.image, (95, 255, 177), (config.WIDTH - 50, 15), (config.WIDTH - 13, 15), 2) # Horizontal Bar
+		pygame.draw.line(weapon_to_display.image, (95, 255, 177), (config.WIDTH - 50, 15), (config.WIDTH - 13, weapon_to_display.rect[1] + weapon_to_display.image.get_rect()[3] + 5 ), 2) # Horizontal Bar
 		
 		
 	def change_items(self):
