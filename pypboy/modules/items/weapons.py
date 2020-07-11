cdfd3fd239ef123b420d2e66c59df2dbde716913
Sergiy Zarubin <36808232+sergiyz57918@users.noreply.github.com>
2020-07-11 16:55:31 -0700

    Update weapons.py

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -12,13 +12,13 @@ class Module(pypboy.SubModule):
 		handlers = []
 		item_names = []
 		INVENTORY = [
-			Weapon('Chinese Assault Rifle','images/inventory/flamer.png',0,0,0,0,''),
-			Weapon('Combat Shotgun','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('Ranger Sequoia','images/inventory/flamer.png',62,30,104,100,''),
+			Weapon('Anti-materiel rifle','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('Deathclaw Gauntlet','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('Flamer','images/inventory/flamer.png',20,10,250,100,''),
 			Weapon('Hunting Rifle','images/inventory/flamer.png',0,0,0,0,''),
-			Weapon('Minigun','images/inventory/flamer.png',0,0,0,0,''),
-			Weapon('Missile Launcher','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('.45-70 Govt(20)','images/inventory/flamer.png',0,0,0,0,''),
+			Weapon('M.50 MG(50)','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('Pulse Grenade (2)','images/inventory/flamer.png',0,0,0,0,'')
 		]
 		selected = 3
