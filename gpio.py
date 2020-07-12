3f79f8c410d8d2c539cb49d9743440c43b0ea215
Sergiy Zarubin <36808232+sergiyz57918@users.noreply.github.com>
2020-07-11 23:31:43 -0700

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -7,7 +7,7 @@ button18 = Button(18)
 button27 = Button(27)
 button22 = Button(22)
 #button23 = Button(23) Reboots system if called pin 16
-button24 = Button(24)
+#button24 = Button(24) No edge detection pin 18
 button25 = Button(25)
 while True:
     
