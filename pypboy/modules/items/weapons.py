0523b9b27224ab0b84ea4b76ca80ab80f12bc709
Sergiy Zarubin <36808232+sergiyz57918@users.noreply.github.com>
2020-07-11 18:46:27 -0700

    Update weapons.py

--- pypboy/modules/items/weapons.py
+++ pypboy/modules/items/weapons.py
@@ -12,10 +12,10 @@ class Module(pypboy.SubModule):
 		handlers = []
 		item_names = []
 		INVENTORY = [
-			Weapon('Ranger Sequoia','images/inventory/flamer.png',62,30,104,100,''),
+			Weapon('Ranger Sequoia','images/inventory/RangerSequoia.png',62,30,104,100,''),
 			Weapon('Anti-materiel rifle','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('Deathclaw Gauntlet','images/inventory/flamer.png',0,0,0,0,''),
-			Weapon('Ranger Sequoia','images/inventory/flamer.png',62,4,250,100,''),
+			Weapon('Ranger Sequoia','images/inventory/RangerSequoia.png',62,4,250,100,''),
 			Weapon('Hunting Rifle','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('.45-70 Govt(20)','images/inventory/flamer.png',0,0,0,0,''),
 			Weapon('M.50 MG(50)','images/inventory/flamer.png',0,0,0,0,''),
