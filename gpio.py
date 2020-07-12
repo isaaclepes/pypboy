e08241c65d4a0eee4b44fda81348b635ecbb67a6
Sergiy Zarubin <36808232+sergiyz57918@users.noreply.github.com>
2020-07-11 23:25:23 -0700

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -13,19 +13,19 @@ while True:
     
     if button4.is_pressed:
         print("4 Pressed pin7")
-    elseif button17.is_pressed:
+    elif button17.is_pressed:
         print("17 Pressed pin 11")
-    elseif button18.is_pressed:
+    elif button18.is_pressed:
         print("18 Pressed pin 12")
-    elseif button27.is_pressed:
+    elif button27.is_pressed:
         print("27 Pressed pin 13")
-    elseif button22.is_pressed:
+    elif button22.is_pressed:
         print("22 Pressed pin 15")
-    elseif button23.is_pressed:
+    elif button23.is_pressed:
         print("23 Pressed pin 16")
-    elseif button24.is_pressed:
+    elif button24.is_pressed:
         print("24 Pressed pin 18")
-    elseif button25.is_pressed:
+    elif button25.is_pressed:
         print("25 Pressed pin 22")
     else:
         print("Released")
