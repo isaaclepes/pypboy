8b5a015cdee74af371af7e382da42f0ca7bdd9ab
kingpinzs <kingpinzs@gmail.com>
2020-07-22 11:19:50 -0600

    make zoom a global

--- pypboy/modules/data/local_map.py
+++ pypboy/modules/data/local_map.py
@@ -12,6 +12,7 @@ class Module(pypboy.SubModule):
 
     def __init__(self, *args, **kwargs):
         super(Module, self).__init__(*args, **kwargs)
+        global zoom
         #mapgrid = entities.MapGrid((-5.9302032, 54.5966701), (config.WIDTH - 8, config.HEIGHT - 80))
         mapgrid = entities.Map(config.WIDTH, pygame.Rect(4, (config.WIDTH - config.HEIGHT) / 2, config.WIDTH - 8, config.HEIGHT - 80))
         if(config.LOAD_CACHED_MAP):
@@ -26,6 +27,7 @@ class Module(pypboy.SubModule):
         mapgrid.rect[1] = 40
     
     def handle_action(self, action):
+        global zoom
         if action == "zoom_in":
             zoom = zoom - 0.003
         if action == "zoom_out":
