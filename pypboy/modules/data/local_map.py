100f9ab3b502233433cccd1c36930dd926371fb8
kingpinzs <kingpinzs@gmail.com>
2020-07-22 11:13:30 -0600

    fix pygame error

--- pypboy/modules/data/local_map.py
+++ pypboy/modules/data/local_map.py
@@ -8,11 +8,12 @@ class Module(pypboy.SubModule):
     label = "Local Map"
     load_cached_map = False
     zoom = 0.003
-    mapgrid = entities.Map(config.WIDTH, pygame.Rect(4, (config.WIDTH - config.HEIGHT) / 2, config.WIDTH - 8, config.HEIGHT - 80))
+    mapgrid = ''
 
     def __init__(self, *args, **kwargs):
         super(Module, self).__init__(*args, **kwargs)
         #mapgrid = entities.MapGrid((-5.9302032, 54.5966701), (config.WIDTH - 8, config.HEIGHT - 80))
+        mapgrid = entities.Map(config.WIDTH, pygame.Rect(4, (config.WIDTH - config.HEIGHT) / 2, config.WIDTH - 8, config.HEIGHT - 80))
         if(config.LOAD_CACHED_MAP):
             print "Loading cached map"
             map_data_location = 'map.cache'
@@ -27,6 +28,6 @@ class Module(pypboy.SubModule):
     def handle_action(self, action):
         if action == "zoom_in":
             zoom = zoom - 0.003
-        if action == "zoom_out"
+        if action == "zoom_out":
             zoom = zoom + 0.003
         mapgrid.load_map(config.MAP_FOCUS, zoom)
